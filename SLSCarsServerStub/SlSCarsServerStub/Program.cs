be9422be8b5b80d908d3ff8954edf4c53bafb4ee
Danik <ld01@tut.by>
2022-11-25 12:48:49 +0300

    inited SlSCarsServerStub

---
+++ SLSCarsServerStub/SlSCarsServerStub/Program.cs
@@ -0,0 +1,22 @@
+namespace SlSCarsServerStub
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseKestrel((builder, options) =>
+                {
+                    options.Configure(
+                        builder.Configuration.GetSection("Kestrel"), reloadOnChange: true);
+                });
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ SLSCarsServerStub/SlSCarsServerStub/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace SlSCarsServerStub
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
