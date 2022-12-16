78133d449af9fd9aea6b59807895476d3dcae8a5
Danik <ld01@tut.by>
2022-12-16 17:27:11 +0300

    added logging

---
+++ SLSCarsServerStub/SlSCarsServerStub/Common/LogFilterAsync.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace SlSCarsServerStub.Common
+{
+    public class LogFilterAsync : IAsyncActionFilter
+    {
+        private static int requestCount_;
+        ILoggerProvider loggerFactory_;
+
+        public LogFilterAsync(ILoggerProvider factory)
+        {
+            loggerFactory_ = factory;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var controller = ((ControllerBase)context.Controller);
+            var requestId = $"{controller.RouteData.Values["action"]}|{Interlocked.Increment(ref requestCount_)}";
+
+            var logger = loggerFactory_.CreateLogger(context.Controller.ToString().Split(".")[^1]);
+
+            var sw = Stopwatch.StartNew();
+
+            logger.LogInformation($"{requestId}({GetArgumentsString(context.ActionArguments)})|Started");
+            try
+            {
+                var response = await next();
+                sw.Stop();
+                logger.LogInformation($"{requestId}|Completed. Elapsed: {sw.Elapsed}. Response: OK");
+
+            }
+            catch (Exception ex)
+            {
+                sw.Stop();
+                logger.LogError(ex, $"{requestId}|Failed. Elapsed: {sw.Elapsed}");
+
+                throw;
+            }
+        }
+
+        private static string GetArgumentsString(IDictionary<string, object> arguments) =>
+            arguments.Any()
+                ? string.Join(',', arguments.Select((pair) => $"{pair.Key}={pair.Value}"))
+                : string.Empty;
+    }
+}
--- SLSCarsServerStub/SlSCarsServerStub/Program.cs
+++ SLSCarsServerStub/SlSCarsServerStub/Program.cs
@@ -1,7 +1,12 @@
+using NLog;
+using NLog.Web;
+
 namespace SlSCarsServerStub
 {
     public class Program
     {
+        private static readonly Logger log_ = LogManager.GetLogger(typeof(Program).FullName);
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -9,6 +14,13 @@ namespace SlSCarsServerStub
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+
+            .ConfigureServices((context, services) =>
+                                {
+                                    TaskScheduler.UnobservedTaskException += (s, a) => { log_.Error($"Task unhandled exception: {a.Exception}"); };
+                                    AppDomain.CurrentDomain.UnhandledException += (s, a) => log_.Error($"Unhandled exception: {a.ExceptionObject}");
+                                })
+            .UseNLog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseKestrel((builder, options) =>
