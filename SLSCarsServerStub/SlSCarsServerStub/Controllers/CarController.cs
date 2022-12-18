b129ef86d2370ab2565e79fb5bf17aa9023b3542
booppush <ld01@tut.by>
2022-12-18 23:45:23 +0300

    add endpoints for dropdown list

--- SLSCarsServerStub/SlSCarsServerStub/Controllers/CarController.cs
+++ SLSCarsServerStub/SlSCarsServerStub/Controllers/CarController.cs
@@ -1,12 +1,19 @@
 ﻿    using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SlSCarsServerStub.Common;
 using SlSCarsServerStub.Models;
+using System.Data.SqlClient;
 using System.Runtime.Serialization.Json;
 
 namespace SlSCarsServerStub.Controllers
 {
     public class CarController : Controller
     {
+        private readonly IConfiguration config_;
+        public CarController(IConfiguration configuration)
+        {
+            config_ = configuration;
+        }
 
         [HttpGet("cars")]
         [TypeFilter(typeof(LogFilterAsync), IsReusable = true)]
@@ -14,6 +21,39 @@ namespace SlSCarsServerStub.Controllers
             vin is null ? brand is null ? GetAllCars() : GetCarByBrand(brand,model) : GetCarByVIN(vin);
 
 
+        [HttpGet("cars/allbrands")]
+        [TypeFilter(typeof(LogFilterAsync), IsReusable = true)]
+        public async Task<IActionResult> GetModels()
+        {
+            var models = new List<string>();
+            var ctx = new SqlConnection(config_.GetConnectionString("sls"));
+            ctx.Open();
+            var command = new SqlCommand("select BrandName from Brands", ctx);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                models.Add((string)reader.GetValue(0));
+            }
+            var result = JsonConvert.SerializeObject(models.ToArray());
+            return Ok(result);
+        }
+
+        [HttpGet("cars/allbrands/{brand}")]
+        [TypeFilter(typeof(LogFilterAsync), IsReusable = true)]
+        public async Task<IActionResult> GetModels(string brand)
+        {
+            var models = new List<string>();
+            var ctx = new SqlConnection(config_.GetConnectionString("sls"));
+            ctx.Open();
+            var command = new SqlCommand($"select ModelName from Models join Brands on Models.BrandID = Brands.ID where BrandName = '{brand}'", ctx);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                models.Add((string)reader.GetValue(0));
+            }
+            var result = JsonConvert.SerializeObject(models.ToArray());
+            return Ok(result);
+        }
         //TODO: return all cars from db
         public IActionResult GetAllCars()
         {
